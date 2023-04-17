1c73e504ce2aa7063958d68183c6cad40b997a87
kuldeep-1024 <kuldeep21328@iiitd.ac.in>
2023-04-17 23:55:50 +0530

    Add files via upload

---
+++ GlobalStyles.js
@@ -0,0 +1,47 @@
+/* fonts */
+export const FontFamily = {
+    poppinsLightRegular: "Poppins Light_regular",
+    poppinsMediumRegular: "Poppins Medium_regular",
+    poppinsMedium: "Poppins_medium",
+    poppinsExtraBoldRegular: "Poppins ExtraBold_regular",
+    poppinsLight: "Poppins_light",
+    poppinsBold: "Poppins_bold",
+  };
+  /* font sizes */
+  export const FontSize = {
+    size_16xl: 35,
+    size_31xl: 50,
+    size_8xl: 27,
+    size_36xl: 55,
+    size_21xl: 40,
+    size_41xl: 60,
+    size_11xl: 30,
+    size_9xl: 28,
+    size_6xl: 25,
+    size_xl: 20,
+    size_3xl: 22,
+  };
+  /* Colors */
+  export const Color = {
+    slategray: "#5a8394",
+    darkslategray: "#323232",
+    white: "#fff",
+    black: "#000",
+    dimgray: "#656565",
+  };
+  /* Paddings */
+  export const Padding = {
+    p_200xl: 219,
+    p_42xl: 61,
+    p_51xl: 70,
+    p_110xl: 129,
+    p_78xl: 97,
+    p_65xl: 84,
+  };
+  /* border radiuses */
+  export const Border = {
+    br_3xs: 10,
+    br_21xl: 40,
+    br_7xl: 26,
+  };
+  
